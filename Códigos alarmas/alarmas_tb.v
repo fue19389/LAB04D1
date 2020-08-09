34d6d147a67b7bfa325397465f16d5cd4762c38e
fue19389 <68131348+fue19389@users.noreply.github.com>
2020-08-09 02:10:52 -0600

    Update alarmas_tb.v

--- Códigos alarmas/alarmas_tb.v
+++ Códigos alarmas/alarmas_tb.v
@@ -13,3 +13,22 @@ module testbench();
   posBVL L4(t1, t2, t3, s4);
   sopBVL L5(t1, t2, t3, s5);
   mmzBVL L6(t1, t2, t3, s6);
+
+  initial begin
+    #1
+    $display("\n");
+    $display("Todas las ecuaciones");
+    $display("A B C | Y1 Y2 Y3 Y4 Y5 Y6");
+    $display("------|------------------");
+    $monitor("%b %b %b | %b  %b  %b  %b  %b  %b", t1, t2, t3, s1, s2, s3, s4 ,s5, s6);
+       t0 = 0; t2 = 0; t3 = 0;
+    #1 t0 = 0; t2 = 0; t3 = 1;
+    #1 t0 = 0; t2 = 1; t3 = 0;
+    #1 t0 = 0; t2 = 1; t3 = 1;
+    #1 t1 = 1; t2 = 0; t3 = 0;
+    #1 t1 = 1; t2 = 0; t3 = 1;
+    #1 t1 = 1; t2 = 1; t3 = 0;
+    #1 t1 = 1; t2 = 1; t3 = 1;
+  end
+
+  
