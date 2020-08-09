0a8f75393d5ffb443ca8be44ab0f29f0e00402aa
fue19389 <68131348+fue19389@users.noreply.github.com>
2020-08-09 02:04:55 -0600

    Update alarmas_tb.v

--- Códigos alarmas/alarmas_tb.v
+++ Códigos alarmas/alarmas_tb.v
@@ -7,4 +7,9 @@ module testbench();
   reg t1, t2, t3;
   wire s1, s2, s3, s4, s5, s6;
 
-  
+  posSTL L1(t1, t2, t3, s1);
+  sopSTL L2(t1, t2, t3, s2);
+  mmzSTL L3(t1, t2, t3, s3);
+  posBVL L4(t1, t2, t3, s4);
+  sopBVL L5(t1, t2, t3, s5);
+  mmzBVL L6(t1, t2, t3, s6);
